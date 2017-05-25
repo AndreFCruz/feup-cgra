1f18bb10aebf71d75b8c70cc2290d99def8b8379
AndreFCruz <andrecruz97@gmail.com>
2017-05-25 11:03:08 +0100

    Added sample video

--- WebCGF/tp6/MySubmarine.js
+++ WebCGF/tp6/MySubmarine.js
@@ -13,6 +13,8 @@ function MySubmarine(scene) {
     this.MAX_VEL = 15; // max Vel in world units per second
     this.velocity = 0;
 
+	this.MIN_HEIGHT = 1;
+	
     this.pos_x = 0;
     this.pos_z = 0;
     this.pos_y = 2; // Altitude
@@ -126,6 +128,9 @@ MySubmarine.prototype.update = function(currTime) {
     this.pos_z += 0.001 * deltaTime * this.velocity * Math.cos(this.ang * this.deg2rad);
     this.pos_y += 0.001 * deltaTime * this.vertical_vel * ((3/4) * (this.velocity / this.MAX_VEL) 
                 + (1/4) * Math.abs(this.theta_ang / this.MAX_THETA_ANG));
+
+    if (this.pos_y < this.MIN_HEIGHT)
+        this.pos_y = this.MIN_HEIGHT;
 
     if (this.velocity != 0)
         this.ang += 0.001 * deltaTime * this.ang_vel * (1 + (this.velocity / this.MAX_VEL));
